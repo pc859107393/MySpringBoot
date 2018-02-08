79e801df48b5b83db2eac4fef56cad98e2389941
cheng <859107393@qq.com>
2018-02-08 16:38:26 +0800

    acheng 创建后端编辑首页

--- src/main/java/acheng1314/cn/controller/admin/SysHomePageController.kt
+++ src/main/java/acheng1314/cn/controller/admin/SysHomePageController.kt
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
@@ -13,5 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 class SysHomePageController {
     @GetMapping(value = ["/banner"], produces = [MediaType.TEXT_HTML_VALUE])
     @ApiOperation(value = "编辑首页轮播图", notes = "首页轮播图编辑器")
-    fun editBanner() = "end/webHome/editBanner"
+    fun editBanner(model: ModelMap) = "end/webHome/editBanner"
+
+
 }
